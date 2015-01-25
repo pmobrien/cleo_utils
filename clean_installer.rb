a4032698575d389cd91c8935dc8082100f11f245
Patrick O'Brien <obrienp@outlook.com>
2015-01-25 00:19:47 -0600

    added command line args to clean installer script

--- clean_installer.rb
+++ clean_installer.rb
@@ -1,18 +1,28 @@
 require "fileutils"
 require "mysql2"
 
-system("sudo dpkg -r couchbase-server")
-system("sudo rm -rf /opt/couchbase")
+def run
+  system("sudo dpkg -r couchbase-server")
+  system("sudo rm -rf /opt/couchbase")
 
-FileUtils.rm_rf("/Users/patrick/Documents/test_boxes/MysqlVagrantBox/unifyrepo")
-FileUtils.rm_rf("/Users/patrick/Documents/test_boxes/MysqlVagrantBox/vltrader")
+  FileUtils.rm_rf($directory + "unifyrepo")
+  FileUtils.rm_rf($directory + "vltrader")
 
-@db_host = "0.0.0.0"
-@db_user = "vltrader"
-@db_pass = "vltrader"
-@db_name = "vltrader"
+  @db_host = "0.0.0.0"
+  @db_user = "vltrader"
+  @db_pass = "vltrader"
+  @db_name = "vltrader"
 
-client = Mysql2::Client.new(:host => @db_host, :username => @db_user, :password => @db_pass)
-client.query("DROP DATABASE IF EXISTS #{@db_name}")
-client.query("CREATE DATABASE #{@db_name}")
-client.close
+  client = Mysql2::Client.new(:host => @db_host, :username => @db_user, :password => @db_pass)
+  client.query("DROP DATABASE IF EXISTS #{@db_name}")
+  client.query("CREATE DATABASE #{@db_name}")
+  client.close
+end
+
+$directory = if(ARGV[0] == nil) then
+  "/vagrant/"
+else
+  ARGV[0]
+end
+
+run()
