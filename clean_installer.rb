4dd0bb7d0e7fed9c40668473969cff7b70414e6b
Patrick O'Brien <obrienp@outlook.com>
2015-01-30 15:41:39 -0600

    small refactor

--- clean_installer.rb
+++ clean_installer.rb
@@ -1,20 +1,25 @@
+require "rubygems"
 require "fileutils"
 require "mysql2"
 
-def run
+def remove_couchbase
   @distro = File.read("/etc/issue")
 
-  if(distro.include?("Ubuntu") || distro.include?("Red Hat"))
+  if(@distro.include?("Ubuntu") || @distro.include?("Red Hat"))
     system("sudo dpkg -r couchbase-server")
     system("sudo rm -rf /opt/couchbase")
-  elsif(distro.include?("CentOS"))
+  elsif(@distro.include?("CentOS"))
     system("sudo rpm -e couchbase-server")
     system("sudo rm -rf /opt/couchbase")
   end
+end
 
+def remove_vltrader
   FileUtils.rm_rf($directory + "unifyrepo")
   FileUtils.rm_rf($directory + "vltrader")
+end
 
+def remove_database
   @db_host = "0.0.0.0"
   @db_user = "vltrader"
   @db_pass = "vltrader"
@@ -26,10 +31,16 @@ def run
   client.close
 end
 
+def run
+  remove_couchbase
+  remove_vltrader
+  remove_database
+end
+
 $directory = if(ARGV[0] == nil) then
   "/vagrant/"
 else
   ARGV[0]
 end
 
-run()
+run
